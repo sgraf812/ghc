1144841fd19a06f64b98eab93b7d5d7bc5ebbab9
Sebastian Graf <sgraf1337@gmail.com>
2017-06-02 14:56:42 +0200

    Crude fix for T11770

--- testsuite/tests/stranal/should_compile/T11770.hs
+++ testsuite/tests/stranal/should_compile/T11770.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE MagicHash #-}
+
 module T11770 where
 
+import GHC.Prim
 
-foo :: Int -> Int ->  Int
-foo 10 c = 0
+
+-- Manually WW'd, so that we explicitly export the worker.
+-- This is so that the non-exported worker won't get one-shot
+-- annotations.
+foo :: Int# -> Int# ->  Int#
+foo 10# c = 0#
 foo n c =
         -- Bar should not be marked as one-shot
-    let bar :: Int -> Int
-        bar n = n + c
+    let bar :: Int# -> Int#
+        bar n = n +# c
         {-# NOINLINE bar #-}
-    in bar n + foo (bar (n+1)) c
+    in bar n +# foo (bar (n+#1#)) c
