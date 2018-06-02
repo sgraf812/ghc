7bf030a165a8aec51297add5a4261131db603e62
Sebastian Graf <sgraf1337@gmail.com>
2018-06-02 18:04:37 +0200

    Temporarily add mk/custom-settings.mk
    According to the wiki page, this is the configuration
    that once passed validate. Let's focus on that.

--- mk/custom-settings.mk
+++ mk/custom-settings.mk
@@ -12,3 +12,6 @@ ifeq "$(BINDIST)" "YES"
 -include bindist.mk
 endif
 
+HADDOCK_DOCS       = NO
+BUILD_SPHINX_HTML  = NO
+GhcLibHcOpts    += -O -dcore-lint  -fllf -fllf-abstract-undersat -fno-llf-abstract-sat -fno-llf-abstract-oversat -fno-llf-create-PAPs -fno-llf-LNE0 -fllf-simpl -fllf-stabilize -fllf-use-strictness -fllf-oneshot -fllf-nonrec-lam-limit=10 -fllf-rec-lam-limit=6
