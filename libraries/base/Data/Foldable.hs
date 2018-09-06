dcb38d4381b4f87aa237c34dc681e9d044a87aa1
Sebastian Graf <sebastian.graf@kit.edu>
2018-09-06 09:41:16 +0200

    Updated Note

--- libraries/base/Data/Foldable.hs
+++ libraries/base/Data/Foldable.hs
@@ -668,30 +668,79 @@ find p = getFirst . foldMap (\ x -> First (if p x then Just x else Nothing))
 {-
 Note [List fusion and continuations in 'c']
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Some list consumers like forM_ are used with huge loop bodies that inhibit the
-expression from properly fusing with a list producer, because the 'c' argument
-is bound to a huge function that can't be inlined multiple times (see
-https://ghc.haskell.org/trac/ghc/ticket/8763#comment:74). Example:
+Suppose we define
+  mapM_ f = foldr ((>>) . f) (return ())
+(this is the way it used to be).
 
-  forM_ (build g) (\n -> huge_body)
-= foldr ((>>) . (\n -> huge_body)) (return ()) (build g)
-= foldr (\x k -> huge_body x >> k) (return ()) (build g)
-= g (\x k -> huge_body x >> k) (return ())
+Now suppose we want to optimise the call
 
-If at this point, 'huge_body' is inlined (single occurrence), we get a lambda
-that is too huge to inline when there are multiple occurences of the 'c'
-parameter of 'g. This spoils recursive join points within 'g' that pass
-themselves as the 'k' parameter to the lambda, which occurs in tail position.
-Inlining the lambda exposes the tail call and GHC recognizes crucial join
-points.
+  mapM_ <big> (build g)
+    where
+  g c n = ...(c x1 y1)...(c x2 y2)....n...
 
-The solution is to give the lambda expression a name ('c', by convention) and
-mark it as INLINE. Crisis averted!
+GHC used to proceed like this:
 
-This is valuable advice everywhere we apply a producer that can't easily honour
-the "single occurrence of 'c'" best practice to a consumer function.
-Sadly, this means all our consumers where the huge body problem might occur have
-to be aware of that.
+  mapM_ <big> (build g)
+
+  = { Defintion of mapM_ }
+    foldr ((>>) . <big>) (return ()) (build g)
+
+  = { foldr/build rule }
+    g ((>>) . <big>) (return ())
+
+  = { Inline g }
+    let c = (>>) . <big>
+        n = return ()
+    in ...(c x1 y1)...(c x2 y2)....n...
+
+The trouble is that `c`, being big, will not be inlined.  And that can
+be absolutely terrible for performance, as we saw in Trac #8763.
+
+It's much better to define
+
+  mapM_ f = foldr c (return ())
+    where
+      c x k = f x >> k
+      {-# INLINE c #-}
+
+Now we get
+  mapM_ <big> (build g)
+
+  = { inline mapM_ }
+    foldr c (return ()) (build g)
+      where c x k = f x >> k
+            {-# INLINE c #-}
+            f = <big>
+
+Notice that `f` does not inline into the RHS of `c`,
+because the INLINE pragma stops it; see
+Note [Simplifying inside stable unfoldings] in SimplUtils.
+Continuing:
+
+  = { foldr/build rule }
+    g c (return ())
+      where ...
+         c x k = f x >> k
+         {-# INLINE c #-}
+            f = <big>
+
+  = { inline g }
+    ...(c x1 y1)...(c x2 y2)....n...
+      where c x k = f x >> k
+            {-# INLINE c #-}
+            f = <big>
+            n = return ()
+
+      Now, crucially, `c` does inline
+
+  = { inline c }
+    ...(f x1 >> y1)...(f x2 >> y2)....n...
+      where f = <big>
+            n = return ()
+
+And all is well!  The key thing is that the fragment
+`(f x1 >> y1)` is inlined into the body of the builder
+`g`.
 -}
 
 {-
