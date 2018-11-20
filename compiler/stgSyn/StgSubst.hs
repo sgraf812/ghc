ac32160065798810916a65a66ee96b615081771a
Sebastian Graf <sebastian.graf@kit.edu>
2018-11-20 16:37:33 +0100

    Comments only

--- compiler/stgSyn/StgSubst.hs
+++ compiler/stgSyn/StgSubst.hs
@@ -12,19 +12,19 @@ import Control.Monad.Trans.State.Strict
 import Outputable
 import Util
 
--- | A renaming substitution from 'Id's to 'Id's. Like 'RnEnv2', but only not
+-- | A renaming substitution from 'Id's to 'Id's. Like 'RnEnv2', but not
 -- maintaining pairs of substitutions. Like @"CoreSubst".'CoreSubst.Subst'@, but
 -- with the domain being 'Id's instead of entire 'CoreExpr'.
 data Subst = Subst InScopeSet IdSubstEnv
 
 type IdSubstEnv = IdEnv Id
 
--- @emptySubst = 'mkEmptySubst' 'emptyInScopeSet'@
+-- | @emptySubst = 'mkEmptySubst' 'emptyInScopeSet'@
 emptySubst :: Subst
 emptySubst = mkEmptySubst emptyInScopeSet
 
--- Constructs a new 'Subst' assuming the variables in the given 'InScopeSet' are
--- in scope.
+-- | Constructs a new 'Subst' assuming the variables in the given 'InScopeSet'
+-- are in scope.
 mkEmptySubst :: InScopeSet -> Subst
 mkEmptySubst in_scope = Subst in_scope emptyVarEnv
 
@@ -46,7 +46,7 @@ substBndr id (Subst in_scope env)
 substBndrs :: Traversable f => f Id -> Subst -> (f Id, Subst)
 substBndrs = runState . traverse (state . substBndr)
 
--- | Substitutes an occurrence of an identifier for it's counterpart recorded
+-- | Substitutes an occurrence of an identifier for its counterpart recorded
 -- in the 'Subst'.
 lookupIdSubst :: HasCallStack => Id -> Subst -> Id
 lookupIdSubst id (Subst in_scope env)
@@ -56,7 +56,7 @@ lookupIdSubst id (Subst in_scope env)
   | otherwise = WARN( True, text "StgSubst.lookupIdSubst" <+> ppr id $$ ppr in_scope)
                 id
 
--- | Substitutes an occurrence of an identifier for it's counterpart recorded
+-- | Substitutes an occurrence of an identifier for its counterpart recorded
 -- in the 'Subst'. Does not generate a debug warning if the identifier to
 -- to substitute wasn't in scope.
 noWarnLookupIdSubst :: HasCallStack => Id -> Subst -> Id
@@ -66,14 +66,14 @@ noWarnLookupIdSubst id (Subst in_scope env)
   | Just id' <- lookupInScope in_scope id = id'
   | otherwise = id
 
--- | Add the 'Var' to the in-scope set: as a side effect,
--- and remove any existing substitutions for it
+-- | Add the 'Id' to the in-scope set and remove any existing substitutions for
+-- it.
 extendInScope :: Id -> Subst -> Subst
 extendInScope id (Subst in_scope env) = Subst (in_scope `extendInScopeSet` id) env
 
 -- | Add a substitution for an 'Id' to the 'Subst': you must ensure that the
 -- in-scope set is such that TyCORep Note [The substitution invariant]
--- holds after extending the substitution like this
+-- holds after extending the substitution like this.
 extendSubst :: Id -> Id -> Subst -> Subst
 extendSubst id new_id (Subst in_scope env)
   = ASSERT2( new_id `elemInScopeSet` in_scope, ppr id <+> ppr new_id $$ ppr in_scope )
